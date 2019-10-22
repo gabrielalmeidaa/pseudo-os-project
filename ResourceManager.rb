d9f7f940fa88468280f484197670187be4728795
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2019-10-21 23:57:51 -0300

    Resource Manager

---
+++ ResourceManager.rb
@@ -0,0 +1,11 @@
+class ResourceManager
+    def initialize
+        # True indicates that the resource is available
+        @scanner = True
+        @printer_1 = True
+        @printer_2 = True
+        @modem = True
+        @sata_1 = True
+        @sata_2 = True
+    end
+end
